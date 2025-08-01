1798893e56fc3aa76dd8509d59fa0fc2cd5f1063
Manuel Samaniego <z23msama@gmail.com>
2025-07-31 21:01:39 -0700

    Fix: Adds use for health on game object

--- game_objects/blueprints/game_object.py
+++ game_objects/blueprints/game_object.py
@@ -1,7 +1,7 @@
 class GameObject:
     def __init__(self, max_health: int) -> None:
-        self.health: int
-        self.max_health: int
+        self.health: int = max_health
+        self.max_health: int = max_health
 
     def take_damage(self, amt: int):
         self.health -= amt
