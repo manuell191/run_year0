b84cff136b343f2f16a57992f32eccb6779144f4
Manuel Samaniego <z23msama@gmail.com>
2025-07-31 17:57:41 -0700

    Feat: Game class
    The Game class deals with all game logic. An introduction function
    was also added to run the introduction to the game and an option to
    add the full game loop later.

---
+++ game.py
@@ -0,0 +1,12 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def introduction(self):
+        # Function to keep introduction organized
+        pass
+
+    def run(self):
+        # Temporary introduction
+        print("Welcome to the world of Run!")
+
