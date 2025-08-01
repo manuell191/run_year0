ad95aaceda247da71eb341fec2cc3038583f85e4
Manuel Samaniego <z23msama@gmail.com>
2025-07-31 18:11:19 -0700

    Fix(game loop): Exit to the game loop
    This change adds an exit to the game loop, something that was
    overlooking during the previous changes.

--- game.py
+++ game.py
@@ -3,14 +3,22 @@ class Game:
         self.running = True
 
     def introduction(self):
-        # Function to keep introduction organized
+        # function to keep introduction organized
         pass
 
+    def process_input(self, user_in):
+        if user_in == "exit":
+            self.running = False
+
     def run(self):
-        # Temporary introduction
+        # temporary introduction
         print("Welcome to the world of Run!")
         
         # regular game loop
         while self.running:
             print("What do you want to do?")
-            input(": ")
+            user_in = input(": ")
+            process_input(user_in)
+
+        # if game ends
+        print("Thanks for playing!")
