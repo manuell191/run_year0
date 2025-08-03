9644d7c62986e5b514d6c88f681dd9d8005e9a0d
Manuel Samaniego <z23msama@gmail.com>
2025-08-03 01:01:33 -0700

    Feat: update GameWeapon
    Update GameWeapon class to be a child class of GameItem

--- game_objects/game_weapon.py
+++ game_objects/game_weapon.py
@@ -1,3 +1,25 @@
-class GameWeapon:
-    def __init__(self):
-        self.damage = [1,2]
+from .blueprints.game_item import GameItem
+
+"""
+Item type:
+
+1: weapon
+2: weapon
+3: weapon
+4: weapon
+5: weapon
+
+"""
+
+class GameWeapon(GameItem):
+    """
+    GameWeapon is a class that holds data for any type of weapon
+
+    Attributes:
+        weght (int): Weight value
+        type (int): A value that explains the type of weapon
+        damage (int): Damage values for the weapon
+    """
+    def __init__(self, item_type: int):
+        super().__init__(item_type)
+        self.damage: list[int] = [1,2]
