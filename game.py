047a138c7546cbaee90caf3a6e1da9679ef6d8e1
Manuel Samaniego <z23msama@gmail.com>
2025-08-03 14:03:51 -0700

    Feat: add introduction to the game

--- game.py
+++ game.py
@@ -8,7 +8,27 @@ class Game:
 
     def introduction(self) -> None:
         # function to keep introduction organized
-        pass
+        print("Welcom to the world of Run!")
+        print("Pick your starting class:")
+        print("1: Warrior\n2: Knight\n3: Thief\n")
+
+        while True:
+            user_in: str = input(": ")
+            user_in = clean_input(user_in)
+
+            if user_in == "1" or user_in == "warrior":
+                self.player = PlayerCharacter(1)
+                break
+            elif user_in == "2" or user_int == "knight":
+                self.player = PlayerCharacter(2)
+                break
+            elif user_in == "3" or user_in == "thief":
+                self.player = PlayerCharacter(3)
+                break
+            else:
+                print("Invalid class. Try again.")
+
+        print("Your adventure now begins")
 
     def process_input(self, user_in: str) -> None:
         # clean input before processing
