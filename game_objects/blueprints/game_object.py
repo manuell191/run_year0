91b22212f84d2e9bfc7e8e1568fb502971d4f269
Manuel Samaniego <z23msama@gmail.com>
2025-08-03 14:42:00 -0700

    Feat: adds xp to game objects

--- game_objects/blueprints/game_object.py
+++ game_objects/blueprints/game_object.py
@@ -5,10 +5,12 @@ class GameObject:
     Attributes:
         max_health (int): maximum health
         health (int): current health
+        xp (int): xp contained (affects xp dropped)
     """
-    def __init__(self, max_health: int) -> None:
+    def __init__(self, max_health: int, xp: int = 0) -> None:
         self.health: int = max_health
         self.max_health: int = max_health
+        self.xp: int = xp
 
     def take_damage(self, amt: int):
         # take damage equal to amount
