cef0c7602fb9eb4ad1b942dc6c4ade2006272cd1
Manuel Samaniego <z23msama@gmail.com>
2025-08-03 00:41:35 -0700

    Feat: GameItem
    The new GameItem class is used to represent any and all game items.
    This class will be used as a parent class but will still need an id
    which will help other parts of the program understand what type of item
    it is.

---
+++ game_objects/blueprints/game_item.py
@@ -0,0 +1,31 @@
+"""
+Item type explainations:
+
+0: any
+1: weapon
+2: weapon
+3: weapon
+4: weapon
+5: weapon
+6: armor
+7: potion
+8: tokens
+TBD
+
+"""
+
+class GameItem:
+    """
+    GameItem is a parent class for multiple types of items.
+
+    Attributes:
+        weight (int): Weight value
+        type (int): A value that explains the type of item
+    """
+    def __init__(self, item_type: int = 0):
+        self.weight: int
+        self.type: int
+
+    def set_weight(self, value: int):
+        # set the weight value for the item
+        self.weight = value
